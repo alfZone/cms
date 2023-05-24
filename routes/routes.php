59c7701665a1f3b2bda1924ed5398f91bb1beeb9
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2023-05-24 12:13:28 +0100

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -9,4 +9,8 @@ Route::get('/manager/articles', function(){  require _CAMINHO_ADMIN. "managerArt
 Route::post('/manager/articles', function(){  require _CAMINHO_ADMIN. "managerArticles.php";});
 Route::get(['set' => '/articles/{cat}/list', 'as' => 'articles.seeArticlesList'], 'ControllerArticles@seeArticlesList');				//WS with Articles List.
 
+//stats
+Route::get(['set' => '/numCategorie', 'as' => 'articles.numCategorie'], 'ControllerArticles@numCategorie');				//number of categories
+Route::get(['set' => '/numArticles', 'as' => 'articles.numArticles'], 'ControllerArticles@numArticles');				//number of articles
+
 //...

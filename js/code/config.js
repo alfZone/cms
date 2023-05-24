6f517631cf9c2e98c69f7c66067a63bc67e5ff07
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2023-05-24 12:12:44 +0100

    Update config.js

--- js/code/config.js
+++ js/code/config.js
@@ -1,1 +1,27 @@
+/**
+ * @author alf
+ * @copyright 2022
+ * @ver 1.0
+ */
 
+
+ class config {
+  
+  
+    constructor() {
+    }
+    
+    color(pos){
+        
+      let cores= ["bg-info", "bg-primary", "bg-success","bg-warning", "bg-danger", "bg-black","bg-indigo", "bg-fuchsia", "bg-navy","bg-info", "bg-primary", "bg-success","bg-warning", "bg-danger", "bg-black","bg-indigo", "bg-fuchsia", "bg-navy"];  
+      if (pos>cores.length){
+        pos=0;
+      }
+      return cores[pos];
+    }
+    
+    
+    get url() {
+      return  window.location.protocol + "//" + window.location.host + "/";
+    }
+  }
